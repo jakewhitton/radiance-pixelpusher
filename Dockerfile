606c2c47fe2ec4f41a00a71d06347d1dafeb31ad
Jay <jaytlang@mit.edu>
2020-01-02 21:56:05 -0500

    removed tetris, set up basics, patched cmakelists.txt

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+# This will be a three stage build.
+# One stage for radiance, one for the layer. 
+# Radiance will start off with qt deps
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "producer/radiance/Radiance.h"
-#include "producer/tetris/Tetris.h"
 #include "consumer/pixelpusher/PixelPusher.h"
 #include "consumer/visualizer/Visualizer.h"
 #include "dancefloor/DanceFloorProgram.h"
@@ -12,16 +11,14 @@ int main()
 {
 	INFO("Issue an EOF (Ctrl + D) to terminate...");
 
-	//const char * radiancePort = "11647";
-	//Radiance producer {radiancePort};
-	
-	Tetris producer;
+	const char * radiancePort = "11647";
+	Radiance producer {radiancePort};
 	
 	const char * pixelPusherLocation = "192.168.0.177";
 	const char * pixelPusherPort = "9897";
-	PixelPusher consumer {pixelPusherLocation, pixelPusherPort};
+	// PixelPusher consumer {pixelPusherLocation, pixelPusherPort};
 	
-	//Visualizer consumer;
+	Visualizer consumer;
 
 	DanceFloorProgram program {producer, consumer};
 
